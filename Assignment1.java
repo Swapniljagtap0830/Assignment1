04322b3f9aa2bc000f0d8d3afd76d102e9f89285
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-07-25 14:35:38 +0530

    Add files via upload
    Java program to find quotient and remainder

---
+++ Assignment1.java
@@ -0,0 +1,15 @@
+
+
+public class Assignment1 {
+
+    public static void main (String[] args)
+     {
+      int dividend = 300, divisor = 250;
+
+      int quotient = dividend / divisor;
+      int remainder = dividend % divisor;
+   
+      System.out.println("The Quotient is=" +quotient);
+      System.out.println("The Remainder is=" +remainder);
+     }
+}
