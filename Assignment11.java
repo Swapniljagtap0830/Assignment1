732bdbef8d609a120a8f7bd8915b497697d9bfc2
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-07-30 15:13:59 +0530

    Add files via upload
    Java program to find factorial

---
+++ Assignment11.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Assignment11 {
+   public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }  
+}
