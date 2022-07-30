e6f7b7957b2800df9f78664a4e2525108b6b89a1
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-07-30 12:21:46 +0530

    Add files via upload
    Java program to add, substract, multiply, and divide two numbers

---
+++ Assignment2.java
@@ -0,0 +1,22 @@
+
+package Assignment;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Assignment2 {
+  public static void main(String[] args)
+  {
+      int a=10 ,b=5,c;
+      c=a+b;
+      System.out.println("addition=" +c);
+      c=a-b;
+      System.out.println("subtract=" +c);
+      c=a*b;
+      System.out.println("multiplication=" +c);
+      c=a/b;
+      System.out.println("division=" +c);   
+  }   
+    
+}
