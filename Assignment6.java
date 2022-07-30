f999aba8fdf1eae7b4e832e7d14bf7a6fb9a546b
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-07-30 15:08:46 +0530

    Add files via upload
    Java program to check whether a character is vowel or consonant

---
+++ Assignment6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Assignment6 {
+    public static void main(String[] args){
+        char ch='i';
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            System.out.println(ch+"is vowel");
+        else
+            System.out.println(ch+"is consonant");
+    }
+}
