cd192809cce3a70cf29df4b81b68a5b8dbe93040
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-07-30 15:12:23 +0530

    Add files via upload
    Java program to calculate sum of natural number

---
+++ Assignment9.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Assignment9  
+{  
+public static void main(String[] args)   
+{  
+int i, num = 10, sum = 0;  
+ 
+for(i = 1; i <= num; ++i)  
+{  
+sum = sum + i;  
+}
+System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+}    
+}
