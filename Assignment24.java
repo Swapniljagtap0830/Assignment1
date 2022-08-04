cb502c31026073992bb6300cbe42c005603c962a
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-08-04 21:18:09 +0530

    Add files via upload
    Java Program to Display Prime Numbers Between Two Intervals

---
+++ Assignment24.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Swapnil
+ */
+public class Assignment24 {
+    public static void main(String[] args) {
+
+        int low = 20, high = 50;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
