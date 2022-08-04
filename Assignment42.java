a0759fefa520a70bc79d409c0de9e4cb529acbdd
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-08-04 21:34:11 +0530

    Add files via upload
    Java Program to Find Largest Element of an Array

---
+++ Assignment42.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Swapnil
+ */
+public class Assignment42 {
+
+    public static void main(String[] args) {
+        double[] numArray = { 23.4, -34.5, 50.0, 33.5, 55.5, 43.7, 5.7, -66.5 };
+        double largest = numArray[0];
+
+        for (double num: numArray) {
+            if(largest < num)
+                largest = num;
+        }
+
+        System.out.format("Largest element = %.2f", largest);
+    }
+}
+
