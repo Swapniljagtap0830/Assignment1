39a970e1f9f0b11c2cfe430a27b61dd6dd9f5a87
Swapniljagtap0830 <109952571+Swapniljagtap0830@users.noreply.github.com>
2022-08-04 21:39:08 +0530

    Add files via upload
    Java Program to Find the Frequency of Characters in a String

---
+++ Assignment50.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Swapnil
+ */
+public class Assignment50 {
+    public static void main(String[] args) {
+        String str = "This website is awesome.";
+        char ch = 'e';
+        int frequency = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(ch == str.charAt(i)) {
+                ++frequency;
+            }
+        }
+
+        System.out.println("Frequency of " + ch + " = " + frequency);
+    }
+}
